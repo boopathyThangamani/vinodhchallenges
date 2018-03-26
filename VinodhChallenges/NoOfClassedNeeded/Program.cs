f4ea384878d2b497fd60364c6aac624d0aabb4fc
boopathyThangamani <boopathii@gmail.com>
2018-03-27 09:12:32 +1100

    Add files via upload

---
+++ VinodhChallenges/NoOfClassedNeeded/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoOfClassedNeeded
+{
+    /*Let X = total no of students
+     T = Seating capacity of a class room.
+     Can u write a function in c# to find out no fo class room required it should handle all scenarios
+         */
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassRoomRequired classRoomRequired = new ClassRoomRequired();
+            Console.Write(classRoomRequired.GetClassRoomsRequired(1, 50));
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    public class ClassRoomRequired
+    {
+        public double GetClassRoomsRequired(double x, double t)
+        {
+            return Math.Ceiling(x / t);
+        }
+    }
+}
